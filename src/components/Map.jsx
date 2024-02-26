0ba50b047fd8a839255938138e8468bafbf2157b
mk <krasickamagdalena@gmail.com>
2024-02-26 17:24:07 +0000

    geolocation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,40 +1,104 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import styles from "./Map.module.css";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
 
 function Map() {
   // eslint-disable-next-line
   const [searchParams, setSearchParams] = useSearchParams();
-  // pragmatic navigation
-  // eslint-disable-next-line
-  const navigate = useNavigate();
+  const { cities } = useCities();
   // eslint-disable-next-line
   const [mapPosition, setMapPosition] = useState([40, 0]);
   // eslint-disable-next-line
-  const lat = searchParams.get("lat");
+  const mapLat = searchParams.get("lat");
   // eslint-disable-next-line
-  const lng = searchParams.get("lng");
+  const mapLng = searchParams.get("lng");
+  // change the map position when the lat and lng change
+  const {
+    isLoading: isLoadingPosition,
+    // eslint-disable-next-line
+    position: geoLocationPosition,
+    getPosition,
+  } = useGeolocation();
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
+
+  useEffect(
+    function () {
+      if (geoLocationPosition) {
+        setMapPosition([geoLocationPosition.lat, geoLocationPosition.lng]);
+      }
+    },
+    [geoLocationPosition]
+  );
+
   return (
     <div className={styles.mapContainer}>
+      {!geoLocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
-        zoom={13}
-        scrollWheelZoom={false}
+        // center={[mapLat, mapLng]}
+        zoom={6}
+        scrollWheelZoom={true}
         className={styles.map}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
+}
+// eslint-disable-next-line
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  // pragmatic navigation
+  // eslint-disable-next-line
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click(e) {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
 
 export default Map;
